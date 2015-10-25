8313c84e275d0e58f9b5bc7b60b63c4cf776a31e
weichnn <weicdai@gmail.com>
2015-10-25 20:11:53 +0800

    init

---
+++ st2icon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Change this to the path to your icns file
+ICON="$HOME/Sublime Text 2.icns"
+
+cp -f "$ICON" "/Applications/Sublime Text 2.app"/Contents/Resources
+cp -r "/Applications/Sublime Text 2.app" "/Applications/Sublime Text 2 copy.app"
+rm -rf "/Applications/Sublime Text 2.app"
+mv "/Applications/Sublime Text 2 copy.app" "/Applications/Sublime Text 2.app"
